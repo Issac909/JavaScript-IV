57b4d5834664b86b373905127c80adfd20863a9d
Issac Moreno <issaxavier909@gmail.com>
2019-10-17 23:03:58 -0700

    MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,170 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr) {
+        this.name = attr.name,
+        this.location - attr.location,
+        this.age = attr.age    
+        
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(iAttr) {
+        super (iAttr)
+        this.specialty = iAttr.specialty,
+        this.favLanguage = iAttr.favLanguage,
+        this.catchPhrase = iAttr.catchPhrase
+    }
+    demo(subject){
+        return `Today we are learning ${subject}`;
+    }
+    grade(name, subject){
+        return `${name} recieves a perfect score on ${subject}.`;
+    }
+}
+
+class Student extends Person {
+    constructor (sAttr) {
+        super(sAttr)
+        this.previousBackground = sAttr.previousBackground,
+        this.className = sAttr.className,
+        this.favSubjects = sAttr.favSubjects
+    }
+    
+    
+    listsSubjects() {
+        return `My favorite subjects are: ${this.favSubjects[0]}, ${this.favSubjects[1]}, and ${this.favSubjects[2]}.`;
+    }
+    PRAssignment(subject){
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor (pmAttr) {
+        super (pmAttr)
+        this.gradClassName = pmAttr.gradClassName,
+        this.favInstructor = pmAttr.favInstructor
+    }
+    standUp(channel, subject) {
+        return `${this.name} announces to ${channel}, @channel standy code on ${subject}`;
+    }
+    debugsCode(name, subject) {
+        return `${this.name} debugs ${name}'s code on ${subject}`;
+    }
+}
+const liz = new ProjectManager ({
+    name: 'Liz',
+    location: 'New York',
+    age: 45,
+    favLanguage: 'Python',
+    specialty: 'User Interface',
+    catchPhrase: 'Never quit!',
+    gradClassName: 'UX08',
+    favInstructor: 'Karin'
+})
+
+const bob = new ProjectManager ({
+    name: 'Bob',
+    location: 'Nevada',
+    age: 50,
+    favLanguage: 'Swift',
+    specialty: 'Data Science',
+    catchPhrase: 'Mistakes are to learn from.',
+    gradClassName: 'DS14',
+    favInstructor: 'Karin'
+})
+
+const jessica = new ProjectManager ({
+    name: 'Jessica',
+    location: 'San Diego',
+    age: 38,
+    favLanguage: 'Java',
+    specialty: 'Software Engeineer',
+    catchPhrase: 'Just keep swimming!',
+    gradClassName: 'CS11',
+    favInstructor: 'Karin'
+})
+
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: `Bedrock`,
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies!`
+  })
+
+  const karin = new Instructor({
+    name: 'Karin',
+    location: 'New Hampshire',
+    age: 28,
+    favLanguage: 'Python',
+    specialty: 'Back-End',
+    catchPhrase: 'It all comes with practice.'
+  })
+
+  const carl = new Instructor({
+    name: 'Carl',
+    location: 'Carlsbad',
+    age: 42,
+    favLanguage: 'C#',
+    specialty: 'Software Applications',
+    catchPhrase: 'Built right. Never rebuild.'
+  })
+
+  const sara = new Student ({
+      name: 'Sara',
+      location: 'Buffalo',
+      age: 22,
+      previousBackground: 'Accounting',
+      className: 'WEB22',
+      favSubjects: [
+          'JavaScript',
+          'Front-End',
+          'Computer Science'
+      ]
+  })
+
+  const peter = new Student ({
+    name: 'Peter',
+    location: 'Quahog',
+    age: 46,
+    previousBackground: 'Fisherman',
+    className: 'RD4848',
+    favSubjects: [
+        'Football',
+        'Seth Macfarlane',
+        'Beer'
+    ]
+})
+
+const craig = new Student ({
+    name: 'Craig',
+    location: 'Ohio',
+    age: 24,
+    previousBackground: 'Robotics',
+    className: 'WEB25',
+    favSubjects: [
+        'Programming',
+        'Web Development',
+        'Computer Science'
+    ]
+})
+
+console.log(fred.speak());
+console.log(fred.demo('Web Development'));
+console.log(craig.listsSubjects());
+console.log(sara.PRAssignment('Front-End'))
+console.log(craig.sprintChallenge('User-Interface'));
+console.log(karin.grade(peter.name, 'JavaScript'));
+console.log(jessica.standUp('WEB22_PR', 'Front-End'));
+console.log(bob.debugsCode(sara.name, 'PRAssignment'));
+console.log(fred.catchPhrase);
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,181 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+
+class GameObject {
+    constructor(attribute){
+    this.createdAt = attribute.createdAt,
+    this.dimensions = attribute.dimensions
+    }
+    destroy(){
+        return `${this.name} has been eliminated.`;
+    }
+  }
+
+
+class CharacterStats extends GameObject {
+    constructor(charAtt) {
+    super(charAtt)
+    this.healthPoints = charAtt.healthPoints,
+    this.name = charAtt.name,
+    this.damage = charAtt.damage,
+    this.secondaryDamage = charAtt.secondaryDamage,
+    this.buffedAttack = charAtt.buffedAttack
+    }
+    takeDamage(){
+        return `${this.name} has been damaged`;
+    }
+  }
+
+ 
+
+class Humanoid extends CharacterStats {
+    constructor(humanAtt) {
+    super(humanAtt)
+    this.team = humanAtt.team,
+    this.weapons = humanAtt.weapons,
+    this.language = humanAtt.language,
+    this.potions = humanAtt.potions,
+    this.healthGain = humanAtt.healthGain,
+    this.secondary = humanAtt.secondary
+    }
+    greet(){
+        return `${this.name} approaches you speaking in ${this.language}`;
+    }
+    attack(){
+        return `${this.name} attacks with ${this.weapons} dealing ${this.damage} damage`;
+    }
+    heal(){
+        return `${this.name} heals for ${this.healthGain} health with ${this.potions}`;
+    }
+    buff(){
+        return `${this.name} drank ${this.potions} and recieved +10 in all stats`;
+    }
+    finisher(){
+        return `${this.name} quickly attacks again with ${this.weapons} and follows up with ${this.secondary} into the chest dealing ${this.buffedAttack + this.secondaryDamage} damage`;
+    }
+    finished(){
+        return `${this.name} and his demon have been conquered`;
+    }
+  }
+
+
+
+  const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 1,
+      height: 1,
+    },
+    healthPoints: 5,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+      'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 2,
+      width: 2,
+      height: 2,
+    },
+    healthPoints: 15,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+      'Giant Sword',
+      'Shield',
+    ],
+    language: 'Common Tongue',
+  });
+
+  const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 10,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+      'Bow',
+      'Dagger',
+    ],
+    language: 'Elvish',
+  });
+
+  const villian = new Humanoid({
+    createdAt: new Date(),
+    dimension: {
+      length: 2,
+      height: 5,
+      width: 3,
+    },
+    healthPoints: 45,
+    name: 'Dr. Piffens',
+    team: "Evil Doers",
+    weapons: [
+      'Giant Hand Saw',
+    ],
+    damage: [
+      25,
+    ],
+    potions: [
+      'Medicine',
+    ],
+    healthGain: 20,
+    language: 'Common Tongue',
+  });
+
+  const hero = new Humanoid({
+    createdAt: new Date(),
+    dimension: {
+      length: 2,
+      height: 6,
+      width: 3,
+    },
+    healthPoints: 30,
+    name: 'Jarvis',
+    team: 'Dark Elimination Forces',
+    weapons: [
+      'Demon Slayer',
+    ],
+    damage: 20,
+    secondary: [
+      'Blessed Dagger',
+    ],
+    secondaryDamage: 15,
+    potions: [
+      'Champions Brew',
+    ],
+    buffedAttack: 30,
+    language: 'Common Tongue',
+  });
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+  console.log(villian.attack());
+  console.log(archer.takeDamage());
+  console.log(archer.destroy());
+  console.log(hero.attack());
+  console.log(villian.takeDamage());
+  console.log(villian.heal());
+  console.log(hero.buff());
+  console.log(villian.attack());
+  console.log(hero.finisher());
+  console.log(villian.finished());
